cf19ecb481aa05a10125f410d2b50a277c8b0ad4
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-01-18 23:07:53 +0100

    corrected

--- mainpanel.py
+++ mainpanel.py
@@ -3,7 +3,7 @@ from neo4j import GraphDatabase
 
 class Connection:
     def __init__(self):
-        self._driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+        self._driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "amalia"))
 
     def getSession(self):
         return self._driver.session()
@@ -40,8 +40,7 @@ class GraphManager:
 
     def add_review(self, nameHotel, nameReviewer, vote):
         ses = self.conn.getSession()
-        ses.run("MATCH (b:Reviewer { name: $nameReviewer }, (a:Hotel { name:$nameHotel }))",
-                "MERGE (b)-[r:REVIEW { vote: $vote }]->(a)", nameHotel=nameHotel, nameReviewer=nameReviewer,
+        ses.run("MATCH (b:Reviewer { name: $nameReviewer }) MATCH ((a:Hotel { name:$nameHotel })) MERGE (b)-[r:REVIEW ]->(a) SET r.vote = $vote", nameHotel=nameHotel, nameReviewer=nameReviewer,
                 vote=vote)
 
     def delete_hotel(self, nameHotel):
@@ -54,11 +53,17 @@ class GraphManager:
 
     def getReviewers(self, hotelName):
         print("TODO") #restituisce gli id dei reviewers
+    def getNode(self):
+        sess=self.conn.getSession()
+        r=sess.run("MATCH(: Hotel {name: 'Residenza'})-[r] - () RETURN r")
+
 
 if __name__ == '__main__':
+
     graph_mg = GraphManager()
     graph_mg.openConnection()
-    graph_mg.create_hotel("Residenza K", "USA", "Chicago")
-    graph_mg.create_reviewer("John")
-    graph_mg.add_review("Residenza K", "John", 4.8)
+    graph_mg.create_hotel("Residenza", "USA", "Chicago")
+    graph_mg.create_reviewer("Jon")
+    graph_mg.add_review("Residenza ", "John", 4.0)
+    graph_mg.getNode()
     graph_mg.closeConnection()

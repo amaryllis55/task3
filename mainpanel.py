bc23777a3516a4b09922f0ff442bd0bc45cb121d
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-01-18 18:32:46 +0100

    dasa

--- mainpanel.py
+++ mainpanel.py
@@ -3,17 +3,17 @@ from neo4j import GraphDatabase
 
 def create_hotel(tx, name, nation, city):
     tx.run("CREATE (a:Hotel {name: $name, city: $city,  nation: $nation}) ",
-           "CREATE CONSTRAINT ON (a:Hotel) ASSERT a.name IS UNIQUE",
            name=name, nation=nation, city=city)
 
 
 def create_reviewer(tx, name):
     tx.run("CREATE (a:Reviewer {name: $name}) ",
-           "CREATE CONSTRAINT ON (a:Hotel) ASSERT a.name IS UNIQUE",
            name=name)
 
 
 def add_review(tx, nameHotel, nameReviewer, vote):
+
+
     tx.run("MATCH (a:Hotel),(b:Reviewer) WHERE a.name = $nameHotel AND b.name = $nameReviewer",
            "CREATE (a)-[r:REVIEW { vote: $vote }]->(b)",
            "RETURN type(r), r.name", nameHotel=nameHotel, nameReviewer=nameReviewer, vote=vote)

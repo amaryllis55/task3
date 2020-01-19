9e85b42069fe40d18cfda2a4ad9dcb79cc8efd05
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-01-19 11:10:33 +0100

    corrected

--- mainpanel.py
+++ mainpanel.py
@@ -40,8 +40,9 @@ class GraphManager:
 
     def add_review(self, nameHotel, nameReviewer, vote):
         ses = self.conn.getSession()
-        ses.run("MATCH (b:Reviewer { name: $nameReviewer }) MATCH ((a:Hotel { name:$nameHotel })) MERGE (b)-[r:REVIEW ]->(a) SET r.vote = $vote", nameHotel=nameHotel, nameReviewer=nameReviewer,
-                vote=vote)
+        ses.run(
+            "MATCH (a:Reviewer), (b:Hotel) WHERE a.name = $nameReviewer AND b.name = $nameHotel   MERGE (a)-[r:REVIEW]->(b) SET r.vote=$vote",
+            nameHotel=nameHotel, nameReviewer=nameReviewer,            vote=vote)
 
     def delete_hotel(self, nameHotel):
         ses = self.conn.getSession()
@@ -51,8 +52,37 @@ class GraphManager:
         ses = self.conn.getSession()
         ses.run(" MATCH (n { name: $nameReviewer })", "DETACH DELETE n", nameHotel=nameReviewer)
 
-    def getReviewers(self, hotelName):
-        print("TODO") #restituisce gli id dei reviewers
+    def seeGraph(self):
+        ses = self.conn.getSession()
+        pg=ses.run("START n=node(*) MATCH (n)-[r]->(m) RETURN n,r,m")
+        print(pg.single()["n"]["name"])
+
+    def dropEverything(self):
+        ses = self.conn.getSession()
+        ses.run("MATCH (n) DETACH DELETE n")
+    def printRelationshipHotel(self, nameHotel):
+        ses=self.conn.getSession()
+        rec=ses.run("MATCH ()-[r:REVIEW]->(b:Hotel) WHERE b.name = $nameHotel RETURN r.vote", nameHotel=nameHotel)
+        for record in rec:
+            print(record)
 
 
 
+
+
+if __name__ == '__main__':
+    graph_mg=GraphManager()
+    graph_mg.openConnection()
+    graph_mg.getSession()
+
+    graph_mg.create_hotel("Hotel", "J", "hf")
+    graph_mg.create_reviewer("Rev")
+    graph_mg.create_reviewer("Pinco")
+    graph_mg.add_review("Hotel", "Rev", 7)
+    graph_mg.add_review("Hotel", "Pinco", 6)
+
+    graph_mg.printRelationshipHotel("Hotel")
+
+
+    graph_mg.dropEverything()
+    graph_mg.closeConnection()
